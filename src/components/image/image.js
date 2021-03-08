a3994428417a641ada94887e20c168bf7434c7d1
Hong Diep <diep@pop-os.localdomain>
2021-03-08 09:55:38 +0700

    create image file

---
+++ src/components/image/image.js
@@ -0,0 +1,6 @@
+import bunbu from '../image/bunbu.png';
+import slide1  from '../image/slide1.png';
+import slide0  from '../image/banner_2.png'; 
+import slide3  from '../image/banner_3.png';  
+
+export { bunbu , slide0 , slide1, slide3};
