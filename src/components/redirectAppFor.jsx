494fa43f9d1f3dce15d9807f81a4a63801860655
Hong Diep <diep@pop-os.localdomain>
2021-03-08 10:49:17 +0700

    refs #1 fix redirect

--- src/components/redirectAppFor.jsx
+++ src/components/redirectAppFor.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import ForgetPass from './forgetPass/forgetPass.js';
+import ForgetPass from './forgetPass/forgetPass.js'
+
  
 export function RedirectAppFor(props) {
   switch(props.statusLogin){
