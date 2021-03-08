e6e45fa5e82aa5e9fa49ca5d9ec4ec260cf153da
Hong Diep <diep@pop-os.localdomain>
2021-03-08 15:26:01 +0700

    refs #7 create axios changePass

--- src/actions/axiosChangePass.js
+++ src/actions/axiosChangePass.js
@@ -1,5 +1,25 @@
 import axios from '../axios.js';
+import GetCookie from '../components/getCookie.js';
 
-export const sendRequestChangePassAPI = () =>{
-  
+export const sendRequestChangePassAPI = (passwordChange,passConfirm,passwordCurrent,setShowSuccess,setShowDanger) =>{
+  return (dispatch) =>{
+    return axios.put('/password', {
+      password: passwordChange,
+      password_confirmation: passConfirm,
+      current_password: passwordCurrent,
+    },
+      {
+        headers: {
+          'access-token': GetCookie('access-token'),
+          uid: GetCookie('uid'),
+          client: GetCookie('client')
+        }
+      }).then(
+        res => {
+          setShowSuccess(true);
+        }
+      ).catch((err) => {
+        setShowDanger(true);
+      })
+  }
 }
