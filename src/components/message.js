3f6127f273cdf81a78b8ad2769d269a2d4c754e1
Hong Diep <diep@pop-os.localdomain>
2021-03-16 13:51:24 +0700

    refs #1 message and placeholder

---
+++ src/components/message.js
@@ -0,0 +1,7 @@
+const messageLogin = 'Your email or password is wrong.Please check it!.';
+const messageForgetPass = 'Your email is not registed !';
+const messageResetPass = 'password and password Confirm are not same!'
+const placeholderEmail = 'Enter your email';
+const placeholderPass = 'Enter your password';
+
+export  {messageLogin, placeholderEmail , placeholderPass,messageForgetPass,messageResetPass};
